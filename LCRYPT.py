864a4a11adeb2087eb8ac2689a3aea5db92181b5
Locotir <71979632+Locotir@users.noreply.github.com>
2024-05-23 12:11:21 +0200

    Boosted Speed Time

--- LCRYPT.py
+++ LCRYPT.py
@@ -1,4 +1,4 @@
-from getch import getch
+import getch
 import pwinput
 import hashlib
 import sys
@@ -7,6 +7,10 @@ import random
 import time
 import shutil
 import tarfile
+import multiprocessing
+from collections import deque
+from io import BytesIO
+
 
 class bcolors:
     PURPLE = '\033[95m' 
@@ -94,11 +98,11 @@ def tar_decompression(file):
         exit()
 
 # function to convert bits to bytes
-def bits_to_bytes(bit_string):
-    return bytes(int(bit_string[i:i+8], 2) for i in range(0, len(bit_string), 8))
-
+def bits_to_bytes(bits):
+    return bytes(int(bits[i:i+8], 2) for i in range(0, len(bits), 8))
+        
 # Function to generate a random sequence based on a password
-def random_secuence(passwd):
+def random_sequence(passwd):
     # Generate first column with password dependency
     first_column = list(range(256))
     passwd_hash = hashlib.sha256(passwd.encode()).digest()
@@ -110,131 +114,218 @@ def random_secuence(passwd):
     second_column = [format(i, '08b') for i in range(256)]
 
     # Combine the two columns into a list
-    random_secuence = [f"{first_column[i]} {second_column[i]}" for i in range(256)]
-    return random_secuence
+    random_sequence = [f"{first_column[i]} {second_column[i]}" for i in range(256)]
+    return random_sequence
 
-def gkey(random_secuence):
-    # Create key in memory
+# Función para leer datos binarios de un archivo en bloques
+def read_binary_in_blocks(file_path, block_size):
+    with open(file_path, "rb") as file:
+        while True:
+            block = file.read(block_size)
+            if not block:
+                break
+            yield block
+
+# Función global para procesar bloques en gkey
+def process_block_gkey(block, key):
+    block_size = 8
+    result = []
+    for byte in block:
+        binary_byte = format(byte, '08b')
+        decimal_value = key.get(binary_byte)
+        if decimal_value is not None:
+            result.append(decimal_value)
+    return result
+
+# Función global para procesar bloques en rkey
+def process_block_rkey(block, inverse_key):
+    result = []
+    for byte in block:
+        binary_block = inverse_key.get(byte, "Unknown")
+        if binary_block != "Unknown":
+            decimal_value = int(binary_block, 2)
+            result.append(decimal_value)
+    return result
+
+# Función para generar la clave en memoria a partir de una secuencia aleatoria
+def generate_key(random_sequence):
     key = {}
-    for line in random_secuence:
+    for line in random_sequence:
         value, binary = line.strip().split()
         key[binary] = int(value)
+    return key
 
-    binary_data = read_binary(target)
 
-    empty_file()
+def gkey(random_sequence, source, target, block_size=1024):
+    # Crear la clave en memoria
+    key = generate_key(random_sequence)
+
+    buffer_size = 500 * 1024 * 1024  # 500 MB
+    buffer = BytesIO()
+
     with open(target, "ab") as encoded_file:
-        if padding != 0:
-            print(bcolors.WHITE + "[" + bcolors.RED + "@" + bcolors.WHITE + "]" + bcolors.WHITE + " Converting bytes to referenced decimal number") 
-        else:
-            print(bcolors.WHITE + "\n[" + bcolors.RED + "@" + bcolors.WHITE + "]" + bcolors.WHITE + " Converting bytes to referenced decimal number") 
-        block_size = 8
-        for i in range(0, len(binary_data), block_size):
-            block = binary_data[i:i+block_size]
-            decimal_value = key.get(block)  
-            decimal_output = str(decimal_value) + "\n"
-            encoded_file.write(bytes([int(decimal_output)]))
+        print(bcolors.WHITE + "\n[" + bcolors.RED + "@" + bcolors.WHITE + "]" + bcolors.WHITE + " Converting bytes to referenced decimal number") 
 
-def rkey(random_secuence):
-    # Create key in memory
-    key = {}
-    for line in random_secuence:
-        value, binary = line.strip().split()
-        key[binary] = int(value)
+        pool = multiprocessing.Pool()
 
-    with open(target, "rb") as encoded_file:
-        # reads content as bytes sequence
-        byte_content = encoded_file.read()
-        decimal_values = [byte for byte in byte_content]
+        for block in read_binary_in_blocks(source, block_size):
+            result = process_block_gkey(block, key)
+            for decimal_value in result:
+                buffer.write(bytes([decimal_value]))
 
+            if buffer.tell() >= buffer_size:
+                buffer.seek(0)
+                encoded_file.write(buffer.read())
+                buffer.seek(0)
+                buffer.truncate(0)
+
+        if buffer.tell() > 0:
+            buffer.seek(0)
+            encoded_file.write(buffer.read())
+
+        pool.close()
+        pool.join()
+
+def rkey(random_sequence, source, target, block_size=1024):
+    # Crear la clave en memoria
+    key = generate_key(random_sequence)
     inverse_key = {value: binary for binary, value in key.items()}
 
-    empty_file()
-    with open(target, "ab") as reconstructed_file:
+    buffer_size = 500 * 1024 * 1024  # 500 MB
+    buffer = BytesIO()
 
+    with open(target, "ab") as reconstructed_file:
         print(bcolors.WHITE + "[" + bcolors.RED + "@" + bcolors.WHITE + "]" + bcolors.WHITE + " Converting decimal number to bytes referenced")
-        for decimal_value in decimal_values:
-            binary_block = inverse_key.get(decimal_value, "Unknown")  
-            if binary_block != "Unknown":
-                decimal_value = int(binary_block, 2) 
-                reconstructed_file.write(bytes([decimal_value]))
+
+        pool = multiprocessing.Pool()
+
+        for block in read_binary_in_blocks(source, block_size):
+            result = process_block_rkey(block, inverse_key)
+            for decimal_value in result:
+                buffer.write(bytes([decimal_value]))
+
+            if buffer.tell() >= buffer_size:
+                buffer.seek(0)
+                reconstructed_file.write(buffer.read())
+                buffer.seek(0)
+                buffer.truncate(0)
+
+        if buffer.tell() > 0:
+            buffer.seek(0)
+            reconstructed_file.write(buffer.read())
+
+        pool.close()
+        pool.join()
+
 
 # Function to invert bits (1 to 0 and 0 to 1)
 def invert_bits(bit_string):
     return ''.join('1' if char == '0' else '0' for char in bit_string)
 
-# Function to generate a random sequence based on a password and length
 def generate_random_sequence(password, length):
-    random.seed(password)
-    return [random.randint(0, length - 1) for _ in range(length)]
+    sha256 = hashlib.sha256()
+    sha256.update(password.encode('utf-8'))
+    digest = sha256.digest()
+    random.seed(digest)
+    return random.sample(range(length), length)
 
-# Function to shuffle a file
-def scramble_file(source, target, password, progress_interval=0.01):
-    total_size = os.path.getsize(source)
-    processed_bytes = 0
-    interval_bytes = max(1, int(total_size * progress_interval))
-    start_time = time.time()
-    
-    with open(source, 'rb') as src, open(target, 'ab') as tgt:
-        byte = src.read(1)
-        while byte:
-            byte_str = bin(int.from_bytes(byte, "big"))[2:].zfill(8)
-            scrambled_byte = process_byte(byte_str, password, scramble=True)
-            tgt.write(bits_to_bytes(scrambled_byte))
-            processed_bytes += 1
-            if processed_bytes % interval_bytes == 0 or processed_bytes == total_size:
-                elapsed_time = time.time() - start_time
-                if elapsed_time > 0:
-                    speed = processed_bytes / elapsed_time
-                    remaining_bytes = total_size - processed_bytes
-                    remaining_time = remaining_bytes / speed
-                else:
-                    remaining_time = 0
-                progress = int(processed_bytes / total_size * 100)
-                print(bcolors.WHITE + "[" + bcolors.RED + "~" + bcolors.WHITE + "]" + bcolors.WHITE +f" Progress: "+bcolors.VIOLET+f"{progress}"+bcolors.WHITE+f"% competed | "+bcolors.BLUEL+f"{processed_bytes}"+bcolors.WHITE+f"/"+bcolors.ORANGE+f"{total_size}"+bcolors.WHITE+f" bytes | Time left: "+bcolors.GREEN+f"{remaining_time:.2f}"+bcolors.WHITE+" sec", end="\r")
-            byte = src.read(1)
+def process_byte_padding(byte, positions):
+    scrambled_byte = ['0'] * 8
+    for i, pos in enumerate(positions):
+        scrambled_byte[i] = ''.join(random.choices('01', k=padding)) + byte[pos]
+    return ''.join(scrambled_byte)
 
-# Function to unshuffle a file
-def unscramble_file(source, target, password, progress_interval=0.01):
-    total_size = os.path.getsize(source)
-    processed_bytes = 0
-    interval_bytes = max(1, int(total_size * progress_interval))
-    start_time = time.time()
-    
-    with open(source, 'rb') as src, open(target, 'ab') as tgt:
-        byte = src.read(1)
-        while byte:
-            byte_str = bin(int.from_bytes(byte, "big"))[2:].zfill(8)
-            unscrambled_byte = process_byte(byte_str, password, scramble=False)
-            tgt.write(bits_to_bytes(unscrambled_byte))
-            processed_bytes += 1
-            if processed_bytes % interval_bytes == 0 or processed_bytes == total_size:
-                elapsed_time = time.time() - start_time
-                if elapsed_time > 0:
-                    speed = processed_bytes / elapsed_time
-                    remaining_bytes = total_size - processed_bytes
-                    remaining_time = remaining_bytes / speed
-                else:
-                    remaining_time = 0
-                progress = int(processed_bytes / total_size * 100)
-                print(bcolors.WHITE + "[" + bcolors.RED + "~" + bcolors.WHITE + "]" + bcolors.WHITE +f" Progress: "+bcolors.VIOLET+f"{progress}"+bcolors.WHITE+f"% competed | "+bcolors.BLUEL+f"{processed_bytes}"+bcolors.WHITE+f"/"+bcolors.ORANGE+f"{total_size}"+bcolors.WHITE+f" bytes | Time left: "+bcolors.GREEN+f"{remaining_time:.2f}"+bcolors.WHITE+" sec", end="\r")
-            byte = src.read(1)
+def process_byte(byte, positions):
+    scrambled_byte = ['0'] * 8
+    for i, pos in enumerate(positions):
+        scrambled_byte[i] = byte[pos]
+    return ''.join(scrambled_byte)
 
-# Function to process a byte based on a password and scramble or unscramble it
-def process_byte(byte_str, password, scramble=True):
-    length = len(byte_str)
-    positions = generate_random_sequence(password, length)
-    processed_byte = list(invert_bits(byte_str))
+def reverse_byte(byte, positions):
+    unscrambled_byte = ['0'] * 8
+    for i, pos in enumerate(positions):
+        unscrambled_byte[pos] = byte[i]
+    return ''.join(unscrambled_byte)
+
+def process_block(args):
+    block_index, block, positions, scramble = args
     if scramble:
-        for i in range(length):
-            pos = positions[i]
-            processed_byte[i], processed_byte[pos] = processed_byte[pos], processed_byte[i]
+        if padding != 0:
+            processed_block = [process_byte_padding(format(byte, '08b'), positions) for byte in block]
+        else:
+            processed_block = [process_byte(format(byte, '08b'), positions) for byte in block]
     else:
-        for i in range(length - 1, -1, -1):
-            pos = positions[i]
-            processed_byte[i], processed_byte[pos] = processed_byte[pos], processed_byte[i]
+        processed_block = [reverse_byte(format(byte, '08b'), positions) for byte in block]
+    return block_index, ''.join(processed_block)
 
-    return ''.join(processed_byte)
+
+# Byte shuffle
+def process_file(source, target, password, scramble=True, progress_interval=0.1, block_size=4096, num_processes=8):
+    total_size = os.path.getsize(source)
+    if padding != 0 and scramble == False:
+        total_size = total_size
+    elif padding != 0 and scramble == True:
+        total_size = total_size * (padding + 1)
+
+    interval_bytes = max(1, int(total_size * progress_interval))
+    start_time = time.time()
+    processed_bytes = 0
+
+    with open(source, 'rb') as src:
+        blocks = []
+        block_index = 0
+        while True:
+            block = src.read(block_size)
+            if not block:
+                break
+            positions = generate_random_sequence(password + str(block_index), 8)
+            blocks.append((block_index, block, positions, scramble))
+            block_index += 1
+
+    with multiprocessing.Pool(processes=num_processes) as pool:
+        processed_blocks = pool.map(process_block, blocks)
+
+    processed_blocks.sort(key=lambda x: x[0])
+
+    buffer = deque()
+    buffer_size = 0
+    max_buffer_size = 500 * 1024 * 1024  # 500MB buffer size
+
+    with open(target, 'ab') as tgt:
+        for block_index, processed_block in processed_blocks:
+            buffer.append(bits_to_bytes(processed_block))
+            buffer_size += len(processed_block) // 8
+
+            if buffer_size >= max_buffer_size:
+                while buffer:
+                    tgt.write(buffer.popleft())
+                buffer_size = 0
+
+            processed_bytes += len(processed_block) // 8
+
+            if processed_bytes >= interval_bytes or processed_bytes == total_size:
+                elapsed_time = time.time() - start_time
+                if elapsed_time > 0:
+                    speed = processed_bytes / elapsed_time
+                    remaining_bytes = total_size - processed_bytes
+                    remaining_time = remaining_bytes / speed
+                else:
+                    remaining_time = 0
+                progress = int(processed_bytes / total_size * 100)
+                print(bcolors.WHITE + "[" + bcolors.RED + "~" + bcolors.WHITE + "]" + bcolors.WHITE +f" Progress: "+bcolors.VIOLET+f"{progress}"+bcolors.WHITE+f"% competed | "+bcolors.BLUEL+f"{processed_bytes}"+bcolors.WHITE+f"/"+bcolors.ORANGE+f"{total_size}"+bcolors.WHITE+f" bytes | Time left: "+bcolors.GREEN+f"{remaining_time:.2f}"+bcolors.WHITE+" sec", end="\r")
+
+                interval_bytes += max(1, int(total_size * progress_interval))
+
+        while buffer:
+            tgt.write(buffer.popleft())
+
+# Function to scramble a file
+def scramble_file(source, target, password, progress_interval=0.01, block_size=4096):
+    process_file(source, target, password, scramble=True, progress_interval=progress_interval, block_size=block_size)
+
+# Function to unscramble a file
+def unscramble_file(source, target, password, progress_interval=0.01, block_size=4096):
+    process_file(source, target, password, scramble=False, progress_interval=progress_interval, block_size=block_size)
 
 
 # Menu
@@ -246,7 +337,7 @@ while choice != "3":
     print(bcolors.RED+"3. Quit")
 
     # Capture user num. input
-    choice = getch()
+    choice = getch.getch()
     if isinstance(choice, bytes):
         choice = choice.decode("utf-8")
 
@@ -268,27 +359,18 @@ while choice != "3":
         tar_compression(target)
 
         print(bcolors.WHITE + "[" + bcolors.RED + "@" + bcolors.WHITE + "]" + bcolors.WHITE + f' Shuffling & Inverting')
-
         # Binary Shuffle | invert | fill | bits->bytes | bytes->decimal
         target_scrambled_file = target+'.temp'
+        hash_obj = hashlib.sha256(password.encode())
+        hash_int = int.from_bytes(hash_obj.digest(), byteorder='big')
         scramble_file(target, target_scrambled_file, password)
         os.replace(target_scrambled_file, target)
 
-        if padding != 0:
-            print(bcolors.WHITE + "\n[" + bcolors.RED + "@" + bcolors.WHITE + "]" + bcolors.WHITE + f' Injecting bits')
-            with open(target, 'rb') as f_input:
-                with open(target + ".temp", 'wb') as f_output:
-                    original_bytes = f_input.read()
-                    bit_string = ''.join(format(byte, '08b') for byte in original_bytes)
-                    modified_bit_string = ''.join(''.join(random.choices('01', k=padding)) + bit for bit in bit_string)
-                    modified_bit_string += ''.join(random.choices('01', k=padding))
-                    f_output.write(bits_to_bytes(''.join(modified_bit_string)))
+        
 
-            # Replace original file
-            os.replace(target + ".temp", target)
-
-        random_secuence = random_secuence(password)
-        gkey(random_secuence)
+        random_sequence = random_sequence(password)
+        gkey(random_sequence, target, target+".temp")
+        os.replace(target+".temp", target)
 
         state = 'without' if padding == 0 else 'with'
         print(bcolors.WHITE+"[" + bcolors.GREEN+"=" + bcolors.WHITE+"]" +bcolors.WHITE+ f" Encrypted file {state} padding & saved as '{target}'.")
@@ -316,12 +398,10 @@ while choice != "3":
         # Recover file in case of unsuccessful decryption
         shutil.copy(target, f"{target}.backup")
 
-        random_secuence = random_secuence(password)
-        rkey(random_secuence)
+        random_sequence = random_sequence(password)
+        rkey(random_sequence, target, target+".temp")
+        os.replace(target+".temp", target)
 
-        with open(target, 'rb') as f:
-            content = f.read()
-        plain = ''.join('1' if bit == '1' else '0' for byte in content for bit in format(byte, '08b'))
 
         def ajustment(data):
             length = len(data)
@@ -334,14 +414,16 @@ while choice != "3":
             with open(target, "wb") as f:
                 f.write(decrypted_bytes)
 
-        if padding != 0:
-            print(bcolors.WHITE + "[" + bcolors.RED + "@" + bcolors.WHITE + "]" + bcolors.WHITE + " Removing random bits")
-            original_data = plain
-            adjusted_data = "".join(original_data[i] for i in range(padding, len(original_data), padding + 1))
-            ajustment(adjusted_data)
-        else:
-            print(bcolors.WHITE + "[" + bcolors.RED + "@" + bcolors.WHITE + "]" + bcolors.WHITE + " Not removing random bits")
-            ajustment(plain)
+        if padding != 0: # Remove bit padding
+            with open(target, 'rb') as f:
+                content = f.read()
+                plain = ''.join('1' if bit == '1' else '0' for byte in content for bit in format(byte, '08b'))
+                
+                print(bcolors.WHITE + "[" + bcolors.RED + "@" + bcolors.WHITE + "]" + bcolors.WHITE + " Removing random bits")
+                original_data = plain
+                adjusted_data = "".join(original_data[i] for i in range(padding, len(original_data), padding + 1))
+                ajustment(adjusted_data)
+
 
         # Unscramble the file
         print(bcolors.WHITE + "[" + bcolors.RED + "@" + bcolors.WHITE + "]" + bcolors.WHITE + ' Inverting & Reverting Binary Shuffle')
